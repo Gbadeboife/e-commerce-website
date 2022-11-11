a93199dc318fd1581e87a38c8bb67438d3960e23
Gbadeboife <85800094+Gbadeboife@users.noreply.github.com>
2022-11-11 10:00:37 +0100

    m

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './components/Home';
+
+
+const App =()=>{
+
+
+  return(
+
+    <div className="App-body">
+      <Home/>
+  </div>
+  )
+}
+
+export default App;
